d8a4fcc920a5d417c3b5fadb08447a9fb70569f0
Chen Katzir <chenka09@gmail.com>
2024-01-22 12:48:55 +0000

    got a coloured brain

--- Q1.m
+++ Q1.m
@@ -14,21 +14,69 @@ figure;
 % Middle slice of the 2nd image volume, which has b=1000
 imshow(flipud(squeeze(dwis(2,:,:,72))'), []);
 
-%%
-
-% Avox = dwis(:,92,65,72);
-Avox = dwis(:,73,110,72);
 
 %%
-images_num = size(Avox,1);
-qx = qhat(1,:);
-qy = qhat(2,:);
-qz = qhat(3,:);
-b = bvals;
+selected_slice = 72;
+img_width = size(dwis,2);
+img_height = size(dwis,3);
 
-Y = [ones(1,images_num); -b.*qx.^2; -2*b.*qx.*qy; -2*b.*qx.*qz; -b.*qy.^2; -2*b.*qy.*qz; -b.*qz.^2]';
+meanDiff_map = zeros(img_width,img_height);
+FA_map = zeros(img_width,img_height);
+dir_map = zeros(img_width,img_height,3);
+
+Y = GetDesignMatrix(qhat,bvals);
+Y_pinv = pinv(Y);
+
+for i=1:img_width
+    for j=1:img_height
+        if min(dwis(:,i,j,selected_slice))<=0
+            continue
+        end
+        D = GetDiffusionMatrixForVoxel(dwis(:,i,j,selected_slice),Y_pinv);
+        meanDiff_map(i,j) = (trace(D))/3;
+        [V,M] = eig(D);
+
+        R = D/trace(D);
+        FA_map(i,j) = sqrt(0.5*(3-1/trace(R^2)));
+
+        % Find the index of the maximum eigenvalue
+        [~, maxEigenvalueIndex] = max(diag(M));
+        
+        % Extract the corresponding eigenvector
+        mainEigenvector = V(:, maxEigenvalueIndex);
+
+        dir_map(i,j,:) = abs(mainEigenvector)*FA_map(i,j);
+    end
+end
+
 %%
-x = Y\log(Avox);
+figure;
+imshow(flipud(meanDiff_map'), []);
+
 %%
+figure;
+imshow(flipud(FA_map'), []);
+
+%%
+figure;
+imshow(flipud(permute(dir_map,[2,1,3])), []);
+%%
+figure;
+quiver3(flipud(permute(dir_map,[2,1,3])));
+
+function Y = GetDesignMatrix(qhat,b)
+    images_num = size(qhat,2);
+    qx = qhat(1,:);
+    qy = qhat(2,:);
+    qz = qhat(3,:);
+    
+    Y = [ones(1,images_num); -b.*qx.^2; -2*b.*qx.*qy; -2*b.*qx.*qz; -b.*qy.^2; -2*b.*qy.*qz; -b.*qz.^2]';
+end
+
+function D = GetDiffusionMatrixForVoxel(Avox,Y_pinv)
+
+x = Y_pinv*log(Avox);
+
 D = [[x(2) x(3) x(4)]; [x(3) x(5) x(6)]; [x(4) x(6) x(7)]];
+end
 
