b2d955243af96213c6d5e5c4a4b7affa78fdcd3a
Chen Katzir <chenka09@gmail.com>
2024-01-29 12:28:26 +0000

    refined code for Q1_2_1 and tested multiple voxels

--- Q1_2_1.m
+++ Q1_2_1.m
@@ -10,13 +10,26 @@ bvals = 1000*sum(qhat.*qhat);
 
 %% perform basic ball and stick fitting
 
+selected_slice = 72;
+% selected_i = 92;
+% selected_j = 65;
+
+% other tested voxels
+% selected_i = 28;
+% selected_j = 61;
+% selected_i = 82;
+% selected_j = 90;
+selected_i = 55;
+selected_j = 100;
+
+
 % number of bootstrap iterations
-N = 200;
+N = 300;
 
 % number of random perturbations
-K = 10; % in section 1.1 we found that 10 get us 95% chances of finding the minimum
+K = 30; % in section 1.1 we found that 10 get us 95% chances of finding the minimum
 
-Avox = dwis(:,92,65,72);
+Avox = dwis(:,selected_i,selected_j,selected_slice);
 
 % number of data samples
 data_size = size(Avox,1);
@@ -32,10 +45,6 @@ for i=1:N
     Avox_sample = Avox(sample_indices);
     qhat_sample = qhat(:,sample_indices);
     bvals_sample = bvals(sample_indices);
-
-    % Avox_sample = Avox;
-    % qhat_sample = qhat;
-    % bvals_sample = bvals;
 
     startx = [3.5e+00 3e-03 2.5e-01 0 0];
 
@@ -85,7 +94,8 @@ format short;
 
 param_names = {'S0','diff','f'};
 
-figure;
+figure('Position',[10 10 1500 300]);
+sgtitle(['Bootstrap results for voxel: [' num2str(selected_i) ', ' num2str(selected_j) ', ' num2str(selected_slice) ']'])
 for i=1:3
     
     sigma = std(results(:,i));
@@ -97,14 +107,16 @@ for i=1:3
     subplot(1,3,i)
     histogram(results(:,i),15, 'Normalization', 'probability');
     title({['Histogram of ' param_names{i}], ...
-        ['2 sigma: [' num2str(mu-2*sigma) ', ' num2str(mu+2*sigma) '] (Blue)'], ...
-        ['95%: [' num2str(per_low) ', ' num2str(per_high) '] (Red)']});
+        ['\mu=' num2str(mu,3) ', \sigma=' num2str(sigma,3)],...
+        ['2\sigma: [' num2str(mu-2*sigma,3) ', ' num2str(mu+2*sigma,3) '], length: ' num2str(4*sigma,3) ' (Blue)'], ...
+        ['95%: [' num2str(per_low,3) ', ' num2str(per_high,3) '], length: ' num2str(per_high-per_low,3) ' (Red)']});
     xlabel(param_names{i})
     hold on;
-    line([per_low, per_low], ylim, 'Color', 'r', 'LineWidth', 2);
-    line([per_high, per_high], ylim, 'Color', 'r', 'LineWidth', 2);
-    line([mu-2*sigma, mu-2*sigma], ylim, 'Color', 'b', 'LineWidth', 2);
-    line([mu+2*sigma, mu+2*sigma], ylim, 'Color', 'b', 'LineWidth', 2);
+    line([per_low, per_low], [0 1], 'Color', 'r', 'LineWidth', 2);
+    line([per_high, per_high], [0 1], 'Color', 'r', 'LineWidth', 2);
+    line([mu-2*sigma, mu-2*sigma], [0 1], 'Color', 'b', 'LineWidth', 2);
+    line([mu+2*sigma, mu+2*sigma], [0 1], 'Color', 'b', 'LineWidth', 2);
+    ylim([0 0.25])
 end
 
 
