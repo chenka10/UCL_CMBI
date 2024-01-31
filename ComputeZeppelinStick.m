f796d49594b8ff352080ffa8d21e9380dd6a859c
Chen Katzir <chenka09@gmail.com>
2024-01-31 18:13:58 +0000

    changed to actual zeppelin and stick...

--- ComputeZeppelinStick.m
+++ ComputeZeppelinStick.m
@@ -10,5 +10,7 @@ phi = x(6);
 % Synthesize the signals according to the model
 fibdir = [cos(phi)*sin(theta) sin(phi)*sin(theta) cos(theta)];
 fibdotgrad = sum(qhat.*repmat(fibdir, [length(qhat) 1])');
-S = S0*(f*exp(-bvals.*(lam2 + (lam1 - lam2)*(fibdotgrad.^2))) + (1-f)*exp(-bvals*lam1));
+S_E = exp(-bvals.*(lam2 + (lam1 - lam2)*(fibdotgrad.^2)));
+S_I = exp(-bvals*lam1.*(fibdotgrad.^2));
+S = S0*(f*S_I + (1-f)*S_E);
 end
